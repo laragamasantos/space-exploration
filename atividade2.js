57543d7d2d4f90780a4dfabd29c2037620339e5d
Lara <laragamasantos@gmail.com>
2023-03-09 11:22:20 -0300

    change + and - symbols

--- atividade2.js
+++ atividade2.js
@@ -4,6 +4,12 @@ function content(e) {
     let clicked = e.currentTarget;
     let parent = clicked.parentNode;
     parent.classList.toggle('expandido');
+
+    if(parent.classList.contains('expandido')){
+        clicked.innerHTML = '-';
+    } else{
+        clicked.innerHTML = '+';
+    }
 }
 
 let itemsEl = document.querySelectorAll('.botao-expandir-retrair');
